1cf64ffee43f678f6c42b5d406e4a77d17dd05a6
Mateusz Pycka <matpyc2@gmail.com>
2019-08-01 10:36:27 +0200

    add basic functions

--- js/script.js
+++ js/script.js
@@ -16,4 +16,51 @@ else if (randomNumber == 3){
 
 printMessage('Mój ruch to: ' + computerMove);
 
-console.log
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+else if (playerInput == 2) {
+  playerMove = 'papier';
+}
+else if (playerInput == 3) {
+  playerMove = "nożyce"
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Wygrałeś!');
+}
+else if (computerMove == 'kamień' && playerMove == 'nożyce') {
+  printMessage('Przegrałeś');
+}
+else if (computerMove == 'kamień' && playerMove == 'kamień') {
+  printMessage('Mamy remis');
+}
+else if (computerMove == 'papier' && playerMove == 'nożyce') {
+  printMessage('Wygrałeś');
+}
+else if (computerMove == 'papier' && playerMove == 'kamień') {
+  printMessage('Przegrałeś');
+}
+else if (computerMove == 'papier' && playerMove == 'papier') {
+  printMessage('Mamy remis');
+}
+else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+  printMessage('Wygrałeś');
+}
+else if (computerMove == 'nożyce' && playerMove == 'papier') {
+  printMessage('Przegrałeś');
+}
+else if (computerMove == 'nożyce' && playerMove == 'nożyce') {
+  printMessage('Mamy remis');
+}
+else if (playerMove == 'nieznany ruch') {
+  printMessage('Wybierz cyfrę od 1 do 3');
+}
