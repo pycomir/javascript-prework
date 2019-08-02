c71daf5e2296d3ce123bcc6eae524af0800a36a1
Mateusz Pycka <matpyc2@gmail.com>
2019-08-02 10:14:54 +0200

    add first function

--- js/script.js
+++ js/script.js
@@ -2,16 +2,20 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-let computerMove = 'nieznany ruch';
+let computerMove = getMoveName(randomNumber);
 
-if(randomNumber == 1){
-  computerMove = 'kamień';
-}
-else if (randomNumber == 2){
-  computerMove = 'papier';
-}
-else if (randomNumber == 3){
-  computerMove = 'nożyce';
+function getMoveName(argMoveId) {
+  if(argMoveId == 1) {
+    return 'kamień';
+  }
+  else if (argMoveId == 2) {
+    return 'papier';
+  }
+  else if (argMoveId == 3) {
+    return 'nożyce';
+  }
+  printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+  return 'nieznany ruch';
 }
 
 printMessage('Mój ruch to: ' + computerMove);
@@ -20,9 +24,9 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
+let playerMove = getMoveName(playerInput);
 
-if(playerInput == '1'){
+/*if(playerInput == '1'){
   playerMove = 'kamień';
 }
 else if (playerInput == 2) {
@@ -30,7 +34,7 @@ else if (playerInput == 2) {
 }
 else if (playerInput == 3) {
   playerMove = "nożyce"
-}
+}*/
 
 printMessage('Twój ruch to: ' + playerMove);
 
